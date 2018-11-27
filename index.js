55e6d23a56f031cd95bf35a81e92a36fab897864
thepersonclass <thepersonclass@gmail.com>
2018-11-27 15:44:32 -0800

    Added token create handler and user read, update, delete

--- index.js
+++ index.js
@@ -105,5 +105,6 @@ var unifiedServer = function(request, response){
 // Define a request router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
